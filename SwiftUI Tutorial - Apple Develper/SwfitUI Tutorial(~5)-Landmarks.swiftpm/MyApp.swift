711aac5c6710396911a206ebef70b07e754121f2
Nak <lyh990408@gmail.com>
2022-02-24 19:43:59 +0900

    Tutorial 5 Compelete

---
+++ SwiftUI Tutorial - Apple Develper/SwfitUI Tutorial(~5)-Landmarks.swiftpm/MyApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct MyApp: App {
+    @StateObject var modelData = ModelData()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(modelData)
+        }
+    }
+}
