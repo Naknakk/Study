69e8f4dc63d4db8e8c0b715fa220ad49b0aa0a08
Nak <lyh990408@gmail.com>
2022-07-08 19:37:34 +0900

    7.8 Answer Modifying

--- Coding Test/Baekjoon/Solving Step by Step/10. Brute Force/4-1018. Redraw Chessboard.swift
+++ Coding Test/Baekjoon/Solving Step by Step/10. Brute Force/4-1018. Redraw Chessboard.swift
@@ -2,5 +2,41 @@ let input = readLine()!.split{$0 == " "}.map{Int(String($0))!}
 let (N, M) = (input[0], input[1])
 let (xRange, yRange) = (0...(M-8), 0...(N-8))
 
-var board = Array(repeating: readLine()!, count: N)
-var boardChecker = Array(repeating: Array(repeating: false, count: M), count: N)
+var board: [[Character]] = []
+for i in 0..<N {
+    board.append(Array(readLine()!))
+}
+
+func toggle(_ check: Character) -> Character {
+    if check == "W" {
+        return "B"
+    } else {
+        return "W"
+    }
+}
+func counter(_ i: Int, _ j: Int) -> Int {
+    var (countW, countB) = (0, 0)
+    var check: Character = "B"
+    for y in i..<(i+8) {
+        check = toggle(check)
+        for x in j..<(j+8) {
+            if board[y][x] != check {
+                countW += 1
+            }else {
+                countB += 1
+            }
+            check = toggle(check)
+        }
+    }
+    return countW > countB ? countB : countW
+}
+var result = 64
+for i in yRange {
+    for j in xRange {
+        var now = counter(i, j)
+        if result > now {
+            result = now
+        }
+    }
+}
+print(result)
