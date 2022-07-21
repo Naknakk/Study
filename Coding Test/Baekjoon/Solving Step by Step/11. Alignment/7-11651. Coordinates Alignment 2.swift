93959bdcebb9ddeed8ca6b91dcc380ce3791fa7a
Nak <lyh990408@gmail.com>
2022-07-21 20:48:28 +0900

    7.15 Baekjoon Solving

---
+++ Coding Test/Baekjoon/Solving Step by Step/11. Alignment/7-11651. Coordinates Alignment 2.swift
@@ -0,0 +1,80 @@
+import Foundation
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let fIO = FileIO()
+
+let N = fIO.readInt()
+var coordinates = Array(repeating: [0, 0], count: N)
+for i in 0..<N {
+    coordinates[i] = [fIO.readInt(), fIO.readInt()]
+}
+func compare(_ a: [Int], _ b: [Int]) -> Bool {
+    if a[1] < b[1] {
+        return true
+    } else if a[1] == b[1] && a[0] < b[0] {
+        return true
+    } else {
+        return false
+    }
+}
+coordinates.sort(by: {compare($0, $1)})
+var str = ""
+for coordinate in coordinates {
+    str += "\(coordinate[0]) \(coordinate[1])\n"
+}
+str.removeLast()
+print(str)
